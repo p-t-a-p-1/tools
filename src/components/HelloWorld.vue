7d277851722c9d417ebaaa239788680107f8b134
p-t-a-p-1 <kb240225+1@gmail.com>
2020-07-08 09:16:24 +0900

    [add] vue cli追加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>{{ test }}</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,9 +36,11 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+    test: process.env.VUE_APP_MOVIE_API_URL
   }
 }
+console.log(process.env.VUE_APP_MOVIE_API_URL)
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
